a7c6878e675ed9045f756cbdc851c9f4a907fd0e
Ye, Weiran <williamwry@gmail.com>
2014-12-22 15:45:03 -0800

    Fixed #5.

--- PackingList/PackingList/TripTypeTableViewController.swift
+++ PackingList/PackingList/TripTypeTableViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class TripTypeTableViewController: UITableViewController {
     
     var _typeSelected:[Bool] = [true, false, false, false]
+    let _prefilledItemsGeneral = ["clothes","money","passport/id","personal hygiene","medicines","first aid","books","chargers","sunglasses"]
+    let _prefilledItemsBusiness = ["business cards","laptop"]
+    let _prefilledItemsFamily = ["snacks","toys","baby monitor","pacifiers","stroller","diapers"]
+    let _prefilledItemsRomantic = ["wine opener","bathing suit","birth control"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,20 +39,16 @@ class TripTypeTableViewController: UITableViewController {
         var packingListViewController = segue.destinationViewController as PackingListTableViewController
         // populate items based on selected trip types
         if _typeSelected[0] { // general
-            packingListViewController._items.append("passport")
-            packingListViewController._items.append("toothbrush")
+            packingListViewController._items += _prefilledItemsGeneral
         }
         if _typeSelected[1] { // business
-            packingListViewController._items.append("business cards")
-            packingListViewController._items.append("laptop")
+            packingListViewController._items += _prefilledItemsBusiness
         }
         if _typeSelected[2] { // family
-            packingListViewController._items.append("snacks")
-            packingListViewController._items.append("toys")
+            packingListViewController._items += _prefilledItemsFamily
         }
         if _typeSelected[3] { // romantic
-            packingListViewController._items.append("wine opener")
-            packingListViewController._items.append("bathing suit")
+            packingListViewController._items += _prefilledItemsRomantic
         }
     }
 
